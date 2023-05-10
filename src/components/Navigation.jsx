c81c0ce22c48c2654ad034c989a831834ee970d5
kenebebh <kenebebhbanigo@gmail.com>
2023-05-10 20:49:59 +0100

    added logo and favicon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,10 +2,15 @@ import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/contact">Contact Us</Link>
-    </nav>
+    <div className="bg-black-100 w-full h-fit py-4 flex justify-between items-center text-white">
+      <figure>
+        <img src="../src/assets/logo.svg" alt="logo" width={200} height={200} />
+      </figure>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact Us</Link>
+      </nav>
+    </div>
   );
 };
 
