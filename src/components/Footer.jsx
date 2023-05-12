6c9ceb2e66e3e95e4c670801f6fa9c800b380bba
kenebebh <kenebebhbanigo@gmail.com>
2023-05-12 06:37:13 +0100

    finished the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,11 @@ const date = new Date();
 const year = date.getFullYear();
 
 const Footer = () => {
-  return <div>&copy; {year} Banigo Kenebebh. All Rights Reserved.</div>;
+  return (
+    <div className="bg-black-100 w-full h-fit py-4 xs:px-4 sm:px-8 flex justify-between items-center text-white fixed bottom-0">
+      &copy; {year} Banigo Kenebebh. All Rights Reserved.
+    </div>
+  );
 };
 
 export default Footer;
